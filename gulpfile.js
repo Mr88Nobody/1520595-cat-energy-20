270b8bd0043d0c4adcf43cbabd39af1dbeb07d50
Mr.Nobody <agwebdes@yandex.ru>
2020-08-18 09:47:55 +0300

    Собираю сборку проект - не собирается

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ const rename = require("gulp-rename");
 const svgstore = require("gulp-svgstore");
 const csso = require("gulp-csso");
 const webp = require("gulp-webp");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+
 
 // Styles
 
@@ -20,19 +23,21 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(sync.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(sync.stream())
 }
-
 exports.styles = styles;
+gulp.task("styles", gulp.series(styles));
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
@@ -40,19 +45,17 @@ const server = (done) => {
   });
   done();
 }
-
 exports.server = server;
+gulp.task ("server", gulp.series(server));
 
-// Watcher
+  //Watcher
 
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
 }
-
-exports.default = gulp.series(
-  styles, server, watcher
-);
+exports.default = gulp.series(styles, server, watcher);
+gulp.task ("watcher", gulp.series(watcher));
 
 // Sprite
 
@@ -60,15 +63,72 @@ const sprite = () => {
   return gulp.src("source/img/**/icon-*.svg")
     .pipe(svgstore())
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"))
-  }
+    .pipe(gulp.dest("build/img"))
+}
 exports.sprite = sprite;
+gulp.task ("sprite", gulp.series(sprite));
 
 // Webp image
+
 
 const createWebp = () => {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"))
-  }
+}
 exports.createWebp = createWebp;
+gulp.task ("createWebp", gulp.series(createWebp));
+
+// Imagemin
+
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+    ]))
+}
+exports.images = images;
+gulp.task ("images", gulp.series(images));
+
+// Copy in build
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"))
+}
+exports.copy = copy;
+gulp.task ("copy", gulp.series(copy));
+
+// clean build
+
+const clean = () => {
+  return del("build")
+}
+exports.clean = clean;
+gulp.task ("clean", gulp.series(clean));
+
+// Build
+const build = (done) => {
+  gulp.series(
+    clean,
+    copy,
+    styles,
+    sprite
+  );
+  done();
+}
+exports.build = build;
+
+gulp.task("build", gulp.series(clean, copy, styles, sprite));
+gulp.task("start", gulp.series(build, server));
